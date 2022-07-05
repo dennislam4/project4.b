5dea5f8d6898a6453331d795ef795aef22d34d3b
dennislam4 <107871794+dennislam4@users.noreply.github.com>
2022-07-05 12:16:21 -0600

    Initial commit

---
+++ fib.py
@@ -0,0 +1,28 @@
+
+# Author: Dennis Lam
+# GitHub username: dennislam4
+# Date: 7/05/2022
+# Description: A function that takes a positive integer parameter and returns the number at that position of
+# the Fibonacci sequence.
+
+def fib(num):
+    p = 1
+    q = 1
+    if num == 1:
+        return 0
+    elif num == 2:
+        return 1
+    else:
+        while num > 2:
+            x = p + q
+            p = q
+            q = x
+            num = num - 1
+
+        return x
+
+
+
+term = fib(17)
+print(term)
+
